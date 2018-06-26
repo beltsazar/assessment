18dfeccd0a77d4c8a9302c4c6087727b129c90e0
Beltsazar <beltsazar.register@gmail.com>
2018-06-26 22:07:10 +0200

    chart.js working

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,136 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
+    <v-app dark>
+        <v-navigation-drawer
+                persistent
+                :mini-variant="miniVariant"
+                :clipped="clipped"
+                v-model="drawer"
+                enable-resize-watcher
+                fixed
+                app
         >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <router-view/>
-    </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
-  </v-app>
+            <v-list>
+                <v-list-tile
+                        value="true"
+                        v-for="(item, i) in items"
+                        :key="i"
+                >
+                    <v-list-tile-action>
+                        <v-icon v-html="item.icon" />
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title v-text="item.title"></v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar
+                app
+                :clipped-left="clipped"
+        >
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-btn icon @click.stop="miniVariant = !miniVariant">
+                <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+            </v-btn>
+            <v-btn icon @click.stop="clipped = !clipped">
+                <v-icon>web</v-icon>
+            </v-btn>
+            <v-btn icon @click.stop="fixed = !fixed">
+                <v-icon>remove</v-icon>
+            </v-btn>
+            <v-toolbar-title v-text="title"></v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+                <v-icon>menu</v-icon>
+            </v-btn>
+        </v-toolbar>
+        <v-content>
+            <router-view/>
+            Chart
+            <canvas id="myChart" xwidth="400" xheight="400"></canvas>
+        </v-content>
+        <v-navigation-drawer
+                temporary
+                :right="right"
+                v-model="rightDrawer"
+                fixed
+                app
+        >
+            <v-list>
+                <v-list-tile @click="right = !right">
+                    <v-list-tile-action>
+                        <v-icon>compare_arrows</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-footer :fixed="fixed" app>
+            <span>&copy; 2017</span>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+    import Chart from 'chart.js'
+
+    export default {
+        data () {
+            return {
+                clipped: false,
+                drawer: true,
+                fixed: false,
+                items: [{
+                    icon: 'bubble_chart',
+                    title: 'Inspire'
+                }],
+                miniVariant: false,
+                right: true,
+                rightDrawer: false,
+                title: 'Vuetify.js'
+            }
+        },
+        name: 'App',
+        mounted: function () {
+            var ctx = document.getElementById('myChart')
+
+            /* eslint-disable no-new */
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    datasets: [{
+                        label: '# of Votes',
+                        data: [12, 19, 3, 5, 2, 3],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            })
+        }
     }
-  },
-  name: 'App'
-}
 </script>
