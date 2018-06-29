12d4be0fb1583323daf60ebc7f4f36f141b557ed
Beltsazar <beltsazar.register@gmail.com>
2018-06-29 16:03:04 +0200

    optimized bar chart component

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -19,99 +19,43 @@
         },
         watch: {
             dataset (data) {
-                if (chart) {
-                    chart.destroy()
-                }
+                chart.data.labels = []
+                chart.data.datasets = []
 
-                chart = new Chart(this.$el, {
-                    type: 'bar',
-                    data: {
-                        labels: data.labels,
-                        datasets: [{
-                            label: data.label,
-                            data: data.data,
-                            backgroundColor: [
-                                'rgba(54, 162, 235, 0.4)'
-                            ],
-                            borderColor: [
-                                'rgba(54, 162, 235, 1)'
-                            ],
-                            borderWidth: 1
-                        }]
-                    },
-                    options: {
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    beginAtZero: true
-                                }
-                            }]
-                        }
-                    }
+                data.labels.map(label => {
+                    chart.data.labels.push(label)
                 })
 
-                // data.labels.map(label => {
-                //     chart.data.labels.push(label)
-                // })
+                chart.data.datasets.push({
+                    label: data.label,
+                    data: data.data,
+                    backgroundColor: [
+                        'rgba(54, 162, 235, 0.4)'
+                    ],
+                    borderColor: [
+                        'rgba(54, 162, 235, 1)'
+                    ],
+                    borderWidth: 1
+                })
 
-                // chart.data.labels.pop();
-                // chart.data.datasets.forEach((dataset) => {
-                //     dataset.data.pop()
-                // })
-                //
-                // chart.update()
-                //
-                // data.labels.map(label => {
-                //     chart.data.labels.push(label)
-                // })
-                //
-                // chart.data.datasets.push({
-                //     label: 'pipo',
-                //     data: [310504, 552339, 259034, 450818, 1231572, 1215966, 641667],
-                //     backgroundColor: [
-                //         'rgba(54, 162, 235, 0.4)'
-                //     ],
-                //     borderColor: [
-                //         'rgba(54, 162, 235, 1)'
-                //     ],
-                //     borderWidth: 1
-                // }
-                // )
-                // chart.data.datasets.forEach((dataset) => {
-                //     dataset.data.push(data)
-                // })
-                // chart.update()
+                chart.update()
             }
         },
         methods: {},
         mounted: function () {
-            // chart = new Chart(this.$el, {
-            //     type: 'bar',
-            //     data: {
-            //         // labels: ['Under 5 Years', '5 to 13 Years', '14 to 17 Years', '18 to 24 Years', '25 to 44 Years', '45 to 64 Years', '65 Years and Over'],
-            //         // datasets: [{
-            //         //     label: 'pipo',
-            //         //     data: [310504, 552339, 259034, 450818, 1231572, 1215966, 641667],
-            //         //     backgroundColor: [
-            //         //         'rgba(54, 162, 235, 0.4)'
-            //         //     ],
-            //         //     borderColor: [
-            //         //         'rgba(54, 162, 235, 1)'
-            //         //     ],
-            //         //     borderWidth: 1
-            //         // }
-            //         // ]
-            //     },
-            //     options: {
-            //         scales: {
-            //             yAxes: [{
-            //                 ticks: {
-            //                     beginAtZero: true
-            //                 }
-            //             }]
-            //         }
-            //     }
-            // })
+            chart = new Chart(this.$el, {
+                type: 'bar',
+                data: {},
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            })
         }
     }
 </script>
